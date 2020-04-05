9f95e7b0542f32dd2ca2283e711ed96daa532a0a
mqqlecule <michaldygaz@protonmail.com>
2020-04-06 01:21:05 +0200

    JQuery update and fetching Token

--- frontend/src/js/login.js
+++ frontend/src/js/login.js
@@ -1,7 +1,25 @@
-function loginSubmit(){
-	var login = $("#input-login").val();
-	var password = $("#input-password").val();
-	var jsonString = JSON.stringify({login: login, password:password});
-	alert(jsonString);
-	return false;
+function loginSubmit() {
+  var login = $("#input-login").val();
+  var password = $("#input-password").val();
+  var jsonString = JSON.stringify({
+    username: login,
+    password: password,
+  });
+
+  $.ajax({
+    type: "POST",
+    url: "http://localhost:8080/token/",
+    data: jsonString,
+    contentType: "application/json",
+    dataType: "json",
+    success: function (data) {
+      console.log(data["token"]);
+      localStorage.setItem("token", data["token"]);
+    },
+    failure: function (errMsg) {
+      alert(errMsg);
+    },
+  });
+
+  return false;
 }
