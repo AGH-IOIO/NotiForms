b7d6801ee09922bdf52439cc72c3a6b4ac1d16cc
Piotr Wróbel <pwrobel5@gmail.com>
2020-04-06 16:21:16 +0200

    fix #19

--- backend/flaskr/users/model.py
+++ backend/flaskr/users/model.py
@@ -1,3 +1,7 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from flaskr.db import db
+
+
 class UserD(object):
     def __init__(self):
         self._username = None
@@ -22,11 +26,14 @@ class UserD(object):
 
     @property
     def password(self):
-        return self._email
+        raise AttributeError('You cannot read password attribute!')
 
     @password.setter
     def password(self, new_password):
-        self._password = new_password
+        self._password = generate_password_hash(new_password)
+
+    def verify_password(self, password):
+        return check_password_hash(self._password, password)
 
     def as_dict(self):
         return {
@@ -34,6 +41,32 @@ class UserD(object):
             "password": self.password,
             "username": self.username
         }
+
+    @staticmethod
+    def list_users(query):
+        return [UserD.from_dict(user) for user in db["users"].find(query)]
+
+    @staticmethod
+    def get_user(query):
+        user_dict = db["users"].find_one(query)
+        if user_dict is None:
+            return None
+
+        return UserD.from_dict(db["users"].find_one(query))
+
+    @staticmethod
+    def create_user(username, email, password):
+        user = UserD.get_user({"$or": [{"username": username}, {"email": email}]})
+        if user is not None:
+            return None
+
+        user = UserD()
+        user.username = username
+        user.email = email
+        user.password = password
+        db["users"].insert_one(user.as_dict())
+
+        return user
 
     @staticmethod
     def from_dict(d):
