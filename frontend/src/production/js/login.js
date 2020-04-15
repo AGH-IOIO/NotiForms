03c2ecf93c88de885515f8fd552b8a48a047ea55
mqqlecule <michaldygaz@protonmail.com>
2020-04-15 17:38:48 +0200

    - Gentellela theme - Login/Register page - Dashboard access requires authentication

---
+++ frontend/src/production/js/login.js
@@ -0,0 +1,54 @@
+function loginSubmit() {
+  var login = $("#login-input-login").val();
+  var password = $("#login-input-password").val();
+  var jsonString = JSON.stringify({
+    username: login,
+    password: password,
+  });
+
+  $.ajax({
+    type: "POST",
+    url: "http://localhost:8080/token/",
+    data: jsonString,
+    contentType: "application/json",
+    dataType: "json",
+    success: function (data) {
+      console.log(data["token"]);
+      localStorage.setItem("token", data["token"]);
+      alert("udalo sie zalogowac");
+      location.href = "/dashboard";
+    },
+    failure: function (errMsg) {
+      console.log(errMsg);
+    },
+  });
+
+  return false;
+}
+
+function registerSubmit() {
+  var login = $("#register-input-login").val();
+  var password = $("#register-input-password").val();
+  var email = $("#register-input-email").val();
+  var jsonString = JSON.stringify({
+    username: login,
+    password: password,
+    email: email,
+  });
+
+  $.ajax({
+    type: "POST",
+    url: "http://localhost:8080/users/",
+    data: jsonString,
+    contentType: "application/json",
+    dataType: "json",
+    success: function (data) {
+      alert("Udalo sie zarejestrowac");
+    },
+    failure: function (errMsg) {
+      console.log(errMsg);
+    },
+  });
+
+  return false;
+}
