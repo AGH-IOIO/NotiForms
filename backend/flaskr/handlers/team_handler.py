df3a8e30106d70218afa5d1f4fdb820d048b9ce1
Piotr Wróbel <pwrobel5@gmail.com>
2020-04-21 11:26:14 +0200

    Close #50 - endpoint for getting team members, for #47 - creating team with given members without asking them to confirm this

--- backend/flaskr/handlers/team_handler.py
+++ backend/flaskr/handlers/team_handler.py
@@ -1,8 +1,10 @@
-from flask import jsonify, Blueprint
+from flask import jsonify, Blueprint, g
 
 from .. import app
-from ..auth import from_jwt
+from ..auth import from_jwt, auth_required
 from ..database.team_dao import TeamDAO
+from ..model.team import Team
+from ..validate import expect_mime, json_body, Validator, mk_error
 
 team_bp = Blueprint('teams', __name__)
 
@@ -21,3 +23,42 @@ def confirm_team(token):
         dao.add_user(username, team_name=team_name)
         return jsonify({"confirmation": "OK"})
     # TODO - redirect somewhere
+
+
+@app.route("/teams/create_team/", methods=["POST"])
+@expect_mime("application/json")
+@json_body
+def create_team_with_users():
+    body = g.body
+    validator = Validator(body)
+
+    validator.field_present("name")
+    validator.field_present("members")
+    error_res = validator.error()
+    if error_res is not None:
+        return error_res
+
+    team_data = {
+        "name": body["name"],
+        "members": body["members"]
+    }
+
+    team = Team(team_data)
+    dao = TeamDAO()
+    if dao.does_team_name_exist(team.name):
+        return mk_error("Team with this name already exists!")
+
+    dao.insert_one(team)
+
+    return jsonify({"confirmation": "OK"})
+
+
+@app.route("/teams/get_members/<team_name>")
+@auth_required
+def get_team_members(team_name):
+    dao = TeamDAO()
+    if not dao.does_team_name_exist(team_name):
+        return mk_error("Team with given name does not exist!")
+
+    members = dao.find_users_from_team(team_name)
+    return jsonify({"members": members})
--- backend/flaskr/tests/test_team_handler.py
+++ backend/flaskr/tests/test_team_handler.py
@@ -10,22 +10,31 @@ from ..model.team import Team
 from ..model.utils import create_team_invitation_for_user_link
 
 
-def test_confirm_invitation(clear_db, flask_client):
-    team_data = {
-        "name": "test_team",
-        "users": []
+@pytest.fixture
+def stub_team():
+    data = {
+        "name": "stub_team",
+        "members": []
     }
-    team = Team(team_data)
+    return data
 
+
+@pytest.fixture
+def stub_user():
     user_data = {
         "username": "new_user",
         "password": "123456789",
         "email": "stubmail@gmail.com"
     }
-    user = User(user_data)
+    return user_data
 
+
+def test_confirm_invitation(clear_db, flask_client, stub_team, stub_user):
     user_dao = UserDAO()
     team_dao = TeamDAO()
+
+    team = Team(stub_team)
+    user = User(stub_user)
 
     user_dao.insert_one(user)
     team_dao.insert_one(team)
@@ -40,3 +49,53 @@ def test_confirm_invitation(clear_db, flask_client):
 
     team_from_db = team_dao.find_one_by_name(team.name)
     assert user.username in team_from_db.members
+
+
+def test_create_team(clear_db, flask_client, stub_team, stub_user, team_data=None):
+    if team_data is None:
+        team_data = stub_team
+        team_data["members"].append(stub_user["username"])
+
+    res = post(flask_client, "/teams/create_team/", team_data)
+    assert res.status_code == 200
+    assert res.get_json()["confirmation"] == "OK"
+
+    team_dao = TeamDAO()
+    team_from_db = team_dao.find_one_by_name(team_data["name"])
+    username = team_data["members"][0]
+    assert team_from_db is not None
+    assert username in team_from_db.members
+
+
+def test_create_team_with_missing_data(clear_db, flask_client):
+    data = {"name": "incorrect_team"}
+    res = post(flask_client, "/teams/create_team/", data)
+    assert res.status_code == 400
+
+
+def test_create_duplicated_team(clear_db, flask_client, stub_team, stub_user):
+    stub_team["members"].append(stub_user["username"])
+    test_create_team(None, flask_client, stub_team, stub_user, stub_team)
+
+    res = post(flask_client, "/teams/create_team/", stub_team)
+    assert res.status_code == 400
+
+
+def test_get_team_members_list(clear_db, flask_client, stub_team):
+    team = Team(stub_team)
+    team.add_user("user1")
+    team.add_user("user2")
+    dao = TeamDAO()
+    dao.insert_one(team)
+
+    res = get_with_auth(flask_client, "/teams/get_members/" + team.name)
+    assert res.status_code == 200
+
+    members_list = res.get_json()["members"]
+    assert "user1" in members_list
+    assert "user2" in members_list
+
+
+def test_get_non_existing_team_members_list(clear_db, flask_client):
+    res = get_with_auth(flask_client, "/teams/get_members/team")
+    assert res.status_code == 400
