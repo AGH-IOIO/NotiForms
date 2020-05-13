56591618f16090fcf17582b04e12cc6115c2bc30
Tomasz Markowski <dzikilamer>
2020-05-13 14:44:25 +0200

    Close #73

--- frontend/src/production/js/pages.js
+++ frontend/src/production/js/pages.js
@@ -12,3 +12,14 @@ function showFormGenerator(){
     $("#main").empty();
     $("#main").load("/production/formgenerator.html");
 }
+
+function showSummary(e){
+	window.summaryJson = '{\
+		"not_filled_yet": ["user1", "user2"],\
+		"questions":[{"type": "open_text", "title": "Pytanie 1"},{"type": "open_text", "title": "Pytanie 2"}],\
+		"answers": [{"username": "user3", "answers": ["Odp 1", "Odp 2"]},{"username": "user4", "answers": ["Odp 3", "Odp 4"]} ]\
+	}';
+
+    $("#main").empty();
+    $("#main").load("/production/summary.html");
+}
