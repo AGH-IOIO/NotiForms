bc2a3dd415bab98f4256015697e5beaa41fbbfa5
Piotr Wróbel <pwrobel5@gmail.com>
2020-05-20 13:31:50 +0200

    Close #113, modified error message

--- backend/flaskr/handlers/template_handler.py
+++ backend/flaskr/handlers/template_handler.py
@@ -46,7 +46,7 @@ def send_forms_to_db(body):
     try:
         results = FormResults(template, team_members, datetime.strptime(body["deadline"], "%Y-%m-%d %H:%M"))
     except ValueError:
-        return mk_error("Error with creating form results object, team members list is not correct")
+        return mk_error("Error with creating form results object, team members list is empty")
 
     results_dao = FormResultsDAO()
     results_dao.insert_one(results)
